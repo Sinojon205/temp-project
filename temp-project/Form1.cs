e1b6b7def22a5738d34ada2d9d6cc4bfe14bba39
Sinojon205 <sinojon205@gmail.com>
2019-12-16 10:35:48 +0500

    added new changes

--- temp-project/Form1.cs
+++ temp-project/Form1.cs
@@ -16,5 +16,10 @@ namespace temp_project
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("File modified");
+        }
     }
 }
